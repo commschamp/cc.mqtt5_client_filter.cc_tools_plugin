7317918a5391ec03dfd24836256a4eafa293d708
Alex Robenko <arobenko@gmail.com>
2024-03-18 17:34:15 +1000

    Requiring v5.2 of the cc_tools_qt.

--- src/Mqtt5ClientFilter.h
+++ src/Mqtt5ClientFilter.h
@@ -33,7 +33,7 @@
 #include <string>
 
 static_assert(CC_MQTT5_CLIENT_MAKE_VERSION(0, 3, 1) <= CC_MQTT5_CLIENT_VERSION, "The version of the cc_mqtt5_client library is too old");
-static_assert(CC_TOOLS_QT_MAKE_VERSION(5, 1, 0) <= CC_TOOLS_QT_VERSION, "The version of the cc_tools_qt library is too old");
+static_assert(CC_TOOLS_QT_MAKE_VERSION(5, 2, 0) <= CC_TOOLS_QT_VERSION, "The version of the cc_tools_qt library is too old");
 
 namespace cc_plugin_mqtt5_client_filter
 {
